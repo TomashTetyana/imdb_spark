c0b354f33752120093e32147db95d9b4f77571e0
TomashTetyana <124676035+TomashTetyana@users.noreply.github.com>
2023-05-02 10:01:56 +0300

    init1_d

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
 import pyspark.sql.types as t
 import pyspark.sql.functions as f
+
 import pandas as pd
 ''''
 conf = SparkConf()
@@ -63,7 +64,7 @@ def main():
                                            t.StructField("job", t.StringType(), True),
                                            t.StructField("characters", t.StringType(), True)])
    f_name = path_dir_in + '/' + 'title.principals.tsv.gz'
-   title_principals = spark_session.read.csv(f_name, header=True, nullValue='null', schema=schema_title_principals, sep='\t')
+   title_principals = spark_session.read.csv(f_name, header=True, schema=schema_title_principals, sep='\t')
 
    schema_title_episode = t.StructType([t.StructField("tconst", t.StringType(), False),
                                         t.StructField("parentTconst", t.StringType(), True),
@@ -91,7 +92,7 @@ def main():
    #df_query1 = title_akas.select("title").where(f.col("region") == "UA")
    #df_query2 = name_basics.select("primaryName", "birthYear").where((f.col("birthYear") >= 1800) & (f.col("birthYear") < 1900))
    #df_query3 = title_basics.select("primaryTitle").where((f.col("titleType") == 'movie') & (f.col("runtimeMinutes") > 120))
-
+   '''
    df_query4_1 = title_principals.drop("ordering","job").where(f.col('category')=='actor')
    df_query4_2 = name_basics.select('nconst','primaryName')
    df_query4_3 = title_basics.select('tconst','primaryTitle').filter((f.col('titleType') == 'movie') |
@@ -100,9 +101,9 @@ def main():
                                                                      (f.col('titleType') == 'tvMiniSeries'))
    df_query4_4 = df_query4_1.join(df_query4_2,df_query4_1.nconst==df_query4_2.nconst,'inner')
    df_query4 = df_query4_4.join(df_query4_3, df_query4_4.tconst == df_query4_3.tconst, 'inner')
-   dr_query4_f = df_query4.select('primaryName','primaryTitle','characters').drop('any')
+   dr_query4_f = df_query4.select('primaryName','primaryTitle','characters').filter(f.col('characters') != '\\N')
    dr_query4_f.show()
-   '''
+   
    df_query5_1 = title_akas.select('titleId','region')
    df_query5_2 = title_basics.select('tconst').where(f.col("isAdult") == 1)
    df_query5_3 = df_query5_2.join(df_query5_1, df_query5_1.titleId == df_query5_2.tconst, 'inner')
@@ -113,7 +114,22 @@ def main():
    df_query6_2 = df_query6_1.join(title_episode, title_episode.parentTconst == df_query6_1.tconst, how = 'inner')
    df_query6_f = df_query6_2.groupby('primaryTitle').count().orderBy('count', ascending=False).limit(100)
    df_query6_f.show()
+
+   df_query7_1 = (title_basics.withColumn('decade',
+                                          f.array(f.floor(f.col('startYear')/10)*10,
+                                                  (f.when(f.col('endYear').isNotNull(),
+                                                          (f.ceil(f.col('endYear')/10)*10-1))
+                                                  .otherwise((f.floor(f.col('startYear')/10)*10+9))))))
+   df_qury7_2 = df_query7_1.select('tconst','primaryTitle','decade')
+   df_query7_3 = df_qury7_2.join(title_ratings, df_qury7_2.tconst == title_ratings.tconst, 'inner')
+   window_dept = Window.partitionBy("decade").orderBy(f.col("averageRating").desc())
+   df_query7_4 = df_query7_3.withColumn("top", f.row_number().over(window_dept))
+   df_query7_5 = df_query7_4.select('primaryTitle','decade','averageRating','numVotes','top').filter(f.col('top') <= 10)
+   df_query7_5.show()
    '''
+
+
+
 
 
 
